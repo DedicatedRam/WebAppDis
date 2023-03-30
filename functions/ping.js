72807640dd6757be8f96970bdec2c842d1086986
DedicatedRam <b9002237@my.shu.ac.uk>
2023-03-30 20:08:09 +0100

    modified:   functions/ping.js

--- functions/ping.js
+++ functions/ping.js
@@ -1,9 +1,10 @@
-export async function getAllDP(context) {
+export async function onRequest(context) {
     await context.env.dataPoints.put(6, Date.now());
     let envkeys = JSON.stringify(await context.env.dataPoints);
     let kList = await context.env.dataPoints.list();
     let keyStr = kList+"";
     //console.log(keyStr);
     const value = await context.env.dataPoints.get("4");
-    return new Response(JSON.stringify(kList));
+    //console.log(value);
+    return new Response(JSON.stringify(kListr));
 }
