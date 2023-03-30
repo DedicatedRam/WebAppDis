d185a6e483b0465172eae4f86c7ecbca7e8d6785
DedicatedRam <b9002237@my.shu.ac.uk>
2023-03-30 13:06:51 +0100

    Get data point 1 test KV

--- functions/data/getDataPoint.js
+++ functions/data/getDataPoint.js
@@ -1,5 +1,8 @@
+export async function onRequest(context){
 const { getNamespace } = require('@cloudflare/kv-namespace');
 const myKVNamespace = getNamespace('dataPoints');
 
-const value = await myKVNamespace.get('my-key');
+const value = await myKVNamespace.get('1');
 console.log(value);
+return new Response(value);
+}
