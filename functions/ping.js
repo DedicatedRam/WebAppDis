ee8dfe53ad44c982cd358ea94a2e8c61efb9d454
DedicatedRam <b9002237@my.shu.ac.uk>
2023-03-30 14:32:28 +0100

    test KV connection

--- functions/ping.js
+++ functions/ping.js
@@ -1,3 +1,4 @@
-export function onRequest(context) {
-    return new Response("pong");
+export async function onRequest(context) {
+    await dataPoints.put(6, Date.now());
+    return new Response(await context.env.dataPoints.list().keys);
 }
