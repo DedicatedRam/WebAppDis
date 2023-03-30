cf07aba151f185d02448fe33078c6825e72d91bd
DedicatedRam <b9002237@my.shu.ac.uk>
2023-03-30 13:47:04 +0100

    KV Cloudflare test read data

--- functions/data/getDataPoint.js
+++ functions/data/getDataPoint.js
@@ -1,8 +1,22 @@
 export async function onRequest(context){
-const { getNamespace } = require('@cloudflare/dataPoints');
+const { getNamespace } = require('@cloudflare/kv-asset-handler');
 const myKVNamespace = getNamespace('dataPoints');
 
 const value = await myKVNamespace.get('1');
 console.log(value);
 return new Response(value);
 }
+
+
+// import { getAssetFromKV } from '@cloudflare/kv-asset-handler';
+
+// // Initialize the KV namespace
+// const namespace = 'dataPoints';
+// const kv = await KVNamespace.create(namespace);
+
+// // Get a value from the KV store
+// const key = '1';
+// const value = await kv.get(key);
+
+// // Log the value to the console
+// console.log(value);
