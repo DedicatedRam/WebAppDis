61d57061e18816cb9f38e541f0b04e76d6554824
DedicatedRam <b9002237@my.shu.ac.uk>
2023-03-30 14:50:32 +0100

    modified:   functions/ping.js

--- functions/ping.js
+++ functions/ping.js
@@ -1,6 +1,6 @@
 export async function onRequest(context) {
     // await context.env.dataPoints.put(6, Date.now());
-    let envkeys = Object.keys(await context.env);
+    let envkeys = Object.keys(await context.env.dataPoints);
     let kList = await context.env.dataPoints.list().keys;
     let keyStr = kList+"";
     //console.log(keyStr);
