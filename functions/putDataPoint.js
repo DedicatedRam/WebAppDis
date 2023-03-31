2b826447e700ee8b799ea553c9698993183e3e81
DedicatedRam <b9002237@my.shu.ac.uk>
2023-03-31 17:41:58 +0100

    modified:   functions/putDataPoint.js

--- functions/putDataPoint.js
+++ functions/putDataPoint.js
@@ -2,6 +2,9 @@ export async function onRequest(context) {
   let kList = (await context.env.dataPoints.list()).keys;
   let numberInList = kList.length;
 
+  let body = readRequestBody(context);
+
+
   console.log(await context);
   
   const corsHeaders = {
@@ -9,6 +12,29 @@ export async function onRequest(context) {
       "Access-Control-Allow-Methods": "GET,HEAD,POST,OPTIONS",
       "Access-Control-Max-Age": "86400",
     }
-  return new Response(JSON.stringify(context), {headers: corsHeaders}
+  return new Response(JSON.stringify(body), {headers: corsHeaders}
   );
+}
+
+
+async function readRequestBody(request) {
+  const contentType = request.headers.get("content-type");
+  if (contentType.includes("application/json")) {
+    return JSON.stringify(await request.json());
+  } else if (contentType.includes("application/text")) {
+    return request.text();
+  } else if (contentType.includes("text/html")) {
+    return request.text();
+  } else if (contentType.includes("form")) {
+    const formData = await request.formData();
+    const body = {};
+    for (const entry of formData.entries()) {
+      body[entry[0]] = entry[1];
+    }
+    return JSON.stringify(body);
+  } else {
+    // Perhaps some other type of data was submitted in the form
+    // like an image, or some other binary data.
+    return "a file";
+  }
 }
