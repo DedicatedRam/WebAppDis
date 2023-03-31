3d72b2df1e292a824c58c2890561f3542391b3ce
DedicatedRam <b9002237@my.shu.ac.uk>
2023-03-31 17:55:05 +0100

    modified:   functions/putDataPoint.js

--- functions/putDataPoint.js
+++ functions/putDataPoint.js
@@ -15,23 +15,7 @@ export async function onRequest(context) {
 }
 
 async function readRequestBody(request) {
-  const contentType = request.headers.get("Content-Type");
-  if (contentType.includes("application/json")) {
+
     return JSON.stringify(await request.json());
-  } else if (contentType.includes("application/text")) {
-    return request.text();
-  } else if (contentType.includes("text/html")) {
-    return request.text();
-  } else if (contentType.includes("form")) {
-    const formData = await request.formData();
-    const body = {};
-    for (const entry of formData.entries()) {
-      body[entry[0]] = entry[1];
-    }
-    return JSON.stringify(body);
-  } else {
-    // Perhaps some other type of data was submitted in the form
-    // like an image, or some other binary data.
-    return "a file";
-  }
+
 }
