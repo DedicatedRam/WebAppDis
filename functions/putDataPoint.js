7800e13a8853ab85061b3c982d991ff9c6179927
DedicatedRam <b9002237@my.shu.ac.uk>
2023-03-31 17:44:28 +0100

    modified:   functions/putDataPoint.js

--- functions/putDataPoint.js
+++ functions/putDataPoint.js
@@ -2,20 +2,17 @@ export async function onRequest(context) {
   let kList = (await context.env.dataPoints.list()).keys;
   let numberInList = kList.length;
 
-  let body = readRequestBody(context);
-
+  let body = await readRequestBody(await context.request);
 
   console.log(await context);
-  
-  const corsHeaders = {
-      "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Methods": "GET,HEAD,POST,OPTIONS",
-      "Access-Control-Max-Age": "86400",
-    }
-  return new Response(JSON.stringify(body), {headers: corsHeaders}
-  );
-}
 
+  const corsHeaders = {
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Methods": "GET,HEAD,POST,OPTIONS",
+    "Access-Control-Max-Age": "86400",
+  };
+  return new Response(JSON.stringify(body), { headers: corsHeaders });
+}
 
 async function readRequestBody(request) {
   const contentType = request.headers.get("content-type");
