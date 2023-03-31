c03ec1203864ff4d159191b4c18a11b61bc4f495
DedicatedRam <b9002237@my.shu.ac.uk>
2023-03-31 13:48:15 +0100

    Fixed missing code

--- functions/getDataPoints.js
+++ functions/getDataPoints.js
@@ -2,7 +2,7 @@ export async function onRequest(context) {
     let kList = await context.env.dataPoints.list();
     let jsnList = [];
     for (let index = 0; index < kList.length; index++) {
-        const element = context.env.dataPoints.get(kList[index]);
+        const element = await context.env.dataPoints.get(kList[index]);
         jsnList.push(element);
     }
     console.log(JSON.stringify(jsnList));
