d5d4de4af79b9d789beb1b615b3b54e24647a93a
DedicatedRam <b9002237@my.shu.ac.uk>
2023-03-31 13:37:00 +0100

    Converting so returns the jsn object

--- functions/getDataPoints.js
+++ functions/getDataPoints.js
@@ -1,4 +1,10 @@
 export async function onRequest(context) {
     let kList = await context.env.dataPoints.list();
-    return new Response(JSON.stringify(kList));
+    let jsnList = [];
+    for (let index = 0; index < kList.length; index++) {
+        const element = kList[index];
+        jsnList.push(element);
+    }
+    console.log(JSON.stringify(jsnList));
+    return new Response(JSON.stringify(jsnList));
 }
