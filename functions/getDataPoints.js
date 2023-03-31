86e7f30e94a3587de78a4d7da7cc253972e390d8
DedicatedRam <b9002237@my.shu.ac.uk>
2023-03-31 15:32:33 +0100

    modified:   functions/getDataPoints.js

--- functions/getDataPoints.js
+++ functions/getDataPoints.js
@@ -2,10 +2,15 @@ export async function onRequest(context) {
     let kList = (await context.env.dataPoints.list()).keys;
     let jsnList = [];
     //let test = await JSON.parse(context.env.dataPoints.get(1));
+    for (let index = 0; index < kList.length; index++) {
+        let id = kList[index].name;
+        let element = JSON.parse(await context.env.dataPoints.get(id));
+        jsnList.push(element);
+    }
     // for(key of kList){
     //     const element = JSON.parse(await context.env.dataPoints.get(key));
     //     jsnList.push(element);
     // }
     // console.log(JSON.stringify(jsnList));
-    return new Response(JSON.stringify(kList));
+    return new Response(JSON.stringify(jsnList));
 }
