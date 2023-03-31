8df41b5188f0519c4a9bb0c9d5e2d3b4f81ac9d8
DedicatedRam <b9002237@my.shu.ac.uk>
2023-03-31 13:54:01 +0100

    test code

--- functions/getDataPoints.js
+++ functions/getDataPoints.js
@@ -1,10 +1,11 @@
 export async function onRequest(context) {
     let kList = await context.env.dataPoints.list();
     let jsnList = [];
+    let test = await context.env.dataPoints.get(1);
     for (let index = 0; index < kList.length; index++) {
         const element = await context.env.dataPoints.get(kList[index]);
         jsnList.push(element);
     }
     console.log(JSON.stringify(jsnList));
-    return new Response(JSON.stringify(jsnList));
+    return new Response(JSON.stringify(test));
 }
