0f9072baa8af2c1d8d8dc5fd2c45b82166668b26
DedicatedRam <b9002237@my.shu.ac.uk>
2023-04-03 11:50:11 +0100

    test

--- mapScript.js
+++ mapScript.js
@@ -119,7 +119,7 @@ function openForm() {
 
 
 async function postJSON(data) {
-  console.log(data);
+  console.log("Data: " + data);
   try {
     const response = await fetch("https://cas-4d0.pages.dev/putDataPoint", {
       method: "POST", // or 'PUT'
