f6a01ceafb31cb4a89b1ad80d8ef2fb0f7559892
DedicatedRam <b9002237@my.shu.ac.uk>
2023-04-04 11:20:42 +0100

    test

--- functions/putDataPoint.js
+++ functions/putDataPoint.js
@@ -2,17 +2,21 @@ export async function onRequest(context) {
   let kList = (await context.env.dataPoints.list()).keys;
   let numberInList = kList.length;
 
-  console.log(await context);
+  const body = await context.request.json();
+  console.log(body);
+
   let res = await context.env.dataPoints.put(numberInList, body);
   console.log(res);
+  
   const headers = {
     Allow: 'OPTIONS, GET, HEAD, POST',
-    'Access-Control-Allow-Origin': 'cas-4d0.pages.dev',
+    'Access-Control-Allow-Origin': 'https://3dee04d9.cas-4d0.pages.dev',
     'Content-type': 'application/json',
   }
-  await context.env.dataPoints.put(numberInList, body);
-  return new  Response(res, { status: 200, headers: headers });
+  
+  return new Response(JSON.stringify(res), { status: 200, headers: headers });
 }
+
 
 
 
