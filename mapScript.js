7394f9493b2da4d6bf4b46eb3bbed53da7266c47
DedicatedRam <b9002237@my.shu.ac.uk>
2023-04-04 15:55:34 +0100

    added day limiter option

--- mapScript.js
+++ mapScript.js
@@ -7,48 +7,64 @@ var map;
 var dayDataPointLimiter = false;
 var mapBounds = [[0, 0], [0.0]];
 function openForm() {
-    document.getElementById("myForm").style.display = "block";
+  document.getElementById("myForm").style.display = "block";
+}
+function closeForm() {
+  document.getElementById("myForm").style.display = "none";
+}
+function devToolsOpen() {
+  document.getElementById("devForm").style.display = "block";
+}
+function devToolsClose() {
+  document.getElementById("devForm").style.display = "none";
+}
+function burgerMenu() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
   }
-  function closeForm() {
-    document.getElementById("myForm").style.display = "none";
+}
+
+function submitUserInputDataPoint() {
+  var title = document.getElementById("userInpTitle").value;
+  var desc = document.getElementById("userInpDesc").value;
+  var type = document.getElementById("eventsType").value;
+  if (
+    title == "" ||
+    desc == "" ||
+    selectedLat == "" ||
+    selectedLong == "" ||
+    type == "dft"
+  ) {
+    alert("You must enter values to be submitted");
   }
-  function devToolsOpen() {
-    document.getElementById("devForm").style.display = "block";
+  if (
+    title != "" &&
+    desc != "" &&
+    selectedLat != "" &&
+    selectedLong != "" &&
+    type != "dft"
+  ) {
+    var geojson = {
+      type: "Feature",
+      properties: {
+        name: title,
+        description: desc,
+        timeCreated: Date.now(),
+        type: type,
+      },
+      geometry: {
+        type: "Point",
+        coordinates: [selectedLong, selectedLat],
+      },
+    };
+
+    postJSON(JSON.stringify(geojson));
+    loadedMarkers.push(geojson);
   }
-  function devToolsClose() {
-    document.getElementById("devForm").style.display = "none";
-  }
-
-
-
-  function submitUserInputDataPoint(){
-    var title = document.getElementById("userInpTitle").value;
-    var desc = document.getElementById("userInpDesc").value;
-    var type = document.getElementById("eventsType").value;
-    if(title == "" || desc == "" || selectedLat == "" || selectedLong == "" || type == "dft"){
-      alert("You must enter values to be submitted");
-    }
-    if(title != "" && desc != "" && selectedLat != "" && selectedLong != "" && type != "dft"){
-        var geojson = {
-            "type": "Feature",
-            "properties": {
-              "name": title,
-              "description": desc,
-              "timeCreated": Date.now(),
-              "type": type
-            },
-            "geometry": {
-              "type": "Point",
-              "coordinates": [selectedLong, selectedLat]
-            }
-          };
-
-          
-          postJSON(JSON.stringify(geojson));
-          loadedMarkers.push(geojson);
-    }
-  }
-
+}
 
 async function postJSON(data) {
   try {
@@ -61,163 +77,168 @@ async function postJSON(data) {
     });
 
     const result = await response.text();
-    if(response.status == 201){
-      populateDataPoints(mapBounds[0][0], mapBounds[0][1], mapBounds[1][0], mapBounds[1][1]);
+    if (response.status == 201) {
+      populateDataPoints(
+        mapBounds[0][0],
+        mapBounds[0][1],
+        mapBounds[1][0],
+        mapBounds[1][1]
+      );
       document.getElementById("userInpTitle").value = "";
       document.getElementById("userInpDesc").value = "";
       document.getElementById("eventsType").value = "dft";
-      document.getElementById("latLabel").innerHTML = "Latitude: "; 
-      document.getElementById("longLabel").innerHTML = "Longitude: "; 
+      document.getElementById("latLabel").innerHTML = "Latitude: ";
+      document.getElementById("longLabel").innerHTML = "Longitude: ";
       alert("Added Successfully");
-    }
-    else{
+    } else {
       new Error(response);
       console.log(response);
     }
-    
   } catch (error) {
     console.error("Error:", error);
   }
 }
 
-
-
-  function errorHandler(error) {
-    console.error(error);
-  }
-  function coOrdListener() { listenForCoOrd = !listenForCoOrd };
-
+function errorHandler(error) {
+  console.error(error);
+}
+function coOrdListener() {
+  listenForCoOrd = !listenForCoOrd;
+}
 
 function initMap() {
-    mapboxgl.accessToken =
-      "pk.eyJ1IjoiaWRsZWdhbWVyIiwiYSI6ImNsNnc0MTNpaDA0dnUzY28xM2NpbWo5NGYifQ.2znpvwwQuZbRG9-uY5Nvhg";
-        map = new mapboxgl.Map({
-      container: "map",
-      style: "mapbox://styles/idlegamer/cl3itqajn008k14rzzjfzcgrk",
-      minZoom: 10,
-      center: [0, 0],
-      projection: "globe",
-    });
-  
-    var geolocate = new mapboxgl.GeolocateControl({
-      positionOptions: {
-        enableHighAccuracy: false,
-      },
-      trackUserLocation: true,
-      showUserHeading: true,
-    });
+  mapboxgl.accessToken =
+    "pk.eyJ1IjoiaWRsZWdhbWVyIiwiYSI6ImNsNnc0MTNpaDA0dnUzY28xM2NpbWo5NGYifQ.2znpvwwQuZbRG9-uY5Nvhg";
+  map = new mapboxgl.Map({
+    container: "map",
+    style: "mapbox://styles/idlegamer/cl3itqajn008k14rzzjfzcgrk",
+    minZoom: 10,
+    center: [0, 0],
+    projection: "globe",
+  });
 
-    
-    map.addControl(geolocate);
-    
-  
-    map.on("click", (e) => {
-        // gets current mouse pointer co ordinates for testing purposes
-        
-        if(listenForCoOrd == false){
-            console.log(e.lngLat.wrap());
-        }
-        if(listenForCoOrd == true){
-           selectedLat = e.lngLat.lat;
-           selectedLong = e.lngLat.lng;
-           document.getElementById("longLabel").innerHTML = 'Longitude: ' + selectedLong;
-           document.getElementById("latLabel").innerHTML = 'Latitude: ' + selectedLat;
-        }
-      });
-    try {
-      geolocate.on("geolocate", function (e) {
-        
-        var lon = e.coords.longitude;
-        var lat = e.coords.latitude;
-        mapBounds = [
-          [lon - 0.0816020798784502, lat - 0.036346035512274], // SouthWest 
-          [lon + 0.0754066900138359, lat + 0.039394074799906], // NorthEast
-        ];
-        map.setMaxBounds(mapBounds);
-        
-        populateDataPoints(mapBounds[0][0], mapBounds[0][1], mapBounds[1][0], mapBounds[1][1]);
-      });
-    } catch (e) {
-      console.log(e);
+  var geolocate = new mapboxgl.GeolocateControl({
+    positionOptions: {
+      enableHighAccuracy: false,
+    },
+    trackUserLocation: true,
+    showUserHeading: true,
+  });
+
+  map.addControl(geolocate);
+
+  map.on("click", (e) => {
+    // gets current mouse pointer co ordinates for testing purposes
+
+    if (listenForCoOrd == false) {
+      console.log(e.lngLat.wrap());
     }
-    
-    
-  
-    map.on("load", () => {
-      map.setFog({});
-    });
-      
-        
+    if (listenForCoOrd == true) {
+      selectedLat = e.lngLat.lat;
+      selectedLong = e.lngLat.lng;
+      document.getElementById("longLabel").innerHTML =
+        "Longitude: " + selectedLong;
+      document.getElementById("latLabel").innerHTML =
+        "Latitude: " + selectedLat;
+    }
+  });
+  try {
+    geolocate.on("geolocate", function (e) {
+      var lon = e.coords.longitude;
+      var lat = e.coords.latitude;
+      mapBounds = [
+        [lon - 0.0816020798784502, lat - 0.036346035512274], // SouthWest
+        [lon + 0.0754066900138359, lat + 0.039394074799906], // NorthEast
+      ];
+      map.setMaxBounds(mapBounds);
 
-            
+      populateDataPoints(
+        mapBounds[0][0],
+        mapBounds[0][1],
+        mapBounds[1][0],
+        mapBounds[1][1]
+      );
+    });
+  } catch (e) {
+    console.log(e);
   }
 
-  function populateDataPoints(SWX, SWY, NEX, NEY){
-    populated = true;
-    if (currentMarkers !=null){
-      for (let index = 0; index < currentMarkers.length; index++) {
-        currentMarkers[index].remove();
-      }
+  map.on("load", () => {
+    map.setFog({});
+  });
+}
+
+function populateDataPoints(SWX, SWY, NEX, NEY) {
+  populated = true;
+  if (currentMarkers != null) {
+    for (let index = 0; index < currentMarkers.length; index++) {
+      currentMarkers[index].remove();
     }
-    fetch('https://cas-4d0.pages.dev/getDataPoints', 
-    {
-      "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Methods": "GET,HEAD,POST,OPTIONS",
-      "Access-Control-Max-Age": "86400",
-    })
-        .then(response => response.json())
-        .then(data => {
-          loadedMarkers = data;
-          loadedMarkers.forEach(e => {
-            var elapsedMinutes = Math.round((Date.now() - e.properties.timeCreated)/(1000*60));
-            var elapsedHours = elapsedMinutes/60;
-            var hoursWhole = Math.floor(elapsedHours);
-            var minDif = elapsedMinutes - (hoursWhole *60);
-            
-            var coords = e.geometry.coordinates;
-            if (((coords[0] > SWX) && (coords[0] < NEX)) && ((coords[1] > SWY) && (coords[1] < NEY))){
+  }
+  fetch("https://cas-4d0.pages.dev/getDataPoints", {
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Methods": "GET,HEAD,POST,OPTIONS",
+    "Access-Control-Max-Age": "86400",
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      loadedMarkers = data;
+      loadedMarkers.forEach((e) => {
+        var elapsedMinutes = Math.round(
+          (Date.now() - e.properties.timeCreated) / (1000 * 60)
+        );
+        var elapsedHours = elapsedMinutes / 60;
+        var hoursWhole = Math.floor(elapsedHours);
+        var minDif = elapsedMinutes - hoursWhole * 60;
+        var coords = e.geometry.coordinates;
+
+        var skipCondition = (dayDataPointLimiter == true) && (minDif => 24);
+
+        if (!skipCondition) {
+          if (
+            coords[0] > SWX &&
+            coords[0] < NEX &&
+            coords[1] > SWY &&
+            coords[1] < NEY
+          ) {
             // create the popup
-            const popup = new mapboxgl.Popup({ offset: 25 })
-            .setHTML('<b>' + e.properties.name+ '</b>' +
-             '<br>' + 
-             e.properties.description +
-             '<br>' +
-             hoursWhole + " hours and " + minDif + " minutes ago");
+            const popup = new mapboxgl.Popup({ offset: 25 }).setHTML(
+              "<b>" +
+                e.properties.name +
+                "</b>" +
+                "<br>" +
+                e.properties.description +
+                "<br>" +
+                hoursWhole +
+                " hours and " +
+                minDif +
+                " minutes ago"
+            );
             let event = e.properties.type;
             // create DOM element for the marker
-            const el = document.createElement('div');
-            if (event =="1"){
+            const el = document.createElement("div");
+            if (event == "1") {
               el.id = "markerFood";
             }
-            if(event == "2"){
+            if (event == "2") {
               el.id = "markerTraffic";
             }
-            if(event == "3"){
+            if (event == "3") {
               el.id = "markerCrime";
             }
-            if(event == "4"){
+            if (event == "4") {
               el.id = "markerSocial";
             }
-    
+
             // create the marker
-            let tempMarker = new mapboxgl.Marker(el).setLngLat(coords).setPopup(popup) // sets a popup on this marker
-            .addTo(map);
+            let tempMarker = new mapboxgl.Marker(el)
+              .setLngLat(coords)
+              .setPopup(popup) // sets a popup on this marker
+              .addTo(map);
             currentMarkers.push(tempMarker);
-          
           }
-
-        });
-
-        })
-        .catch(error => console.error(error));
-      
-  }
-
-  function myFunction() {
-    var x = document.getElementById("myLinks");
-    if (x.style.display === "block") {
-      x.style.display = "none";
-    } else {
-      x.style.display = "block";
-    }
-  }  
+        }
+      });
+    })
+    .catch((error) => console.error(error));
+}
