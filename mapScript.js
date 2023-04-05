ca6b8d11657bd1a20f3c3f879b8383cd6aadd754
DedicatedRam <b9002237@my.shu.ac.uk>
2023-04-05 14:32:32 +0100

    test for new nav bar

--- mapScript.js
+++ mapScript.js
@@ -42,6 +42,12 @@ function burgerMenu() {
   }
 }
 
+function menuOnClick() {
+  document.getElementById("menu-bar").classList.toggle("change");
+  document.getElementById("nav").classList.toggle("change");
+  document.getElementById("menu-bg").classList.toggle("change-bg");
+}
+
 
 function submitUserInputDataPoint() {
   var title = document.getElementById("userInpTitle").value;
