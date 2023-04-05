c83d8eb5aebcce33dae9991e310dc2ebe3dfa83b
DedicatedRam <b9002237@my.shu.ac.uk>
2023-04-05 15:18:14 +0100

    test

--- mapScript.js
+++ mapScript.js
@@ -46,7 +46,6 @@ function menuOnClick() {
   document.getElementById("menu-bar").classList.toggle("change");
   document.getElementById("nav").classList.toggle("change");
   document.getElementById("menu-bg").classList.toggle("change-bg");
-  console.log(document.getElementById("menu-bg"));
 }
 
 
