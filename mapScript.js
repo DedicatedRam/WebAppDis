c09e7a684035d33aa6afb6410b908343ccaa9eca
DedicatedRam <b9002237@my.shu.ac.uk>
2023-04-06 09:46:37 +0100

    testing geolocate alternative

--- mapScript.js
+++ mapScript.js
@@ -226,6 +226,28 @@ function initMap() {
     );
     });
 
+
+navigator.geolocation.getCurrentPosition(function(position) {
+  console.log("Latitude: " + position.coords.latitude);
+  console.log("Longitude: " + position.coords.longitude);
+}, function(error) {
+  switch(error.code) {
+    case error.PERMISSION_DENIED:
+      console.log("User denied the request for Geolocation.");
+      break;
+    case error.POSITION_UNAVAILABLE:
+      console.log("Location information is unavailable.");
+      break;
+    case error.TIMEOUT:
+      console.log("The request to get user location timed out.");
+      break;
+    case error.UNKNOWN_ERROR:
+      console.log("An unknown error occurred.");
+      break;
+  }
+});
+
+
   
   var geolocate = new mapboxgl.GeolocateControl({
     positionOptions: {
