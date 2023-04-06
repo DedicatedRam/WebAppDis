760e7b1f5db4d1c0fc20389a34b0d442a2d1899c
DedicatedRam <b9002237@my.shu.ac.uk>
2023-04-06 12:11:03 +0100

    change where the layer is added

--- mapScript.js
+++ mapScript.js
@@ -257,14 +257,7 @@ function addAnimatedLocToMap(la, lo){
   ]
   }
   });
-  map.addLayer({
-  'id': 'layer-with-pulsing-dot',
-  'type': 'symbol',
-  'source': 'dot-point',
-  'layout': {
-  'icon-image': 'pulsing-dot'
-  }
-  });
+
 }
 
 function initMap() {
@@ -324,6 +317,16 @@ function initMap() {
     },
     labelLayerId
     );
+
+    map.addLayer({
+      'id': 'layer-with-pulsing-dot',
+      'type': 'symbol',
+      'source': 'dot-point',
+      'layout': {
+      'icon-image': 'pulsing-dot'
+      }
+      });
+
     });
 
 
