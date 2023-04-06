3ee841baeb62b2da5db81ada651fd9562a3618cb
DedicatedRam <b9002237@my.shu.ac.uk>
2023-04-06 16:49:45 +0100

    Clean up post request in back

--- functions/putDataPoint.js
+++ functions/putDataPoint.js
@@ -1,5 +1,4 @@
 export async function onRequest(context) {
-  let temp=null;
   try {
     if (context.request.method === 'OPTIONS') {
       // respond to preflight request
@@ -12,23 +11,15 @@ export async function onRequest(context) {
     } else if (context.request.method === 'POST') {
       let kList = (await context.env.dataPoints.list()).keys;
       let numberInList = kList.length;
-
       const body = await context.request.json();
-      temp = await context.request;
-      console.log(body);
-
-      let res = await context.env.dataPoints.put(numberInList, body);
-      console.log(res);
-
+      await context.env.dataPoints.put(numberInList, body);
       const headers = {
         'Access-Control-Allow-Origin': '*',
         'Content-Type': 'application/json',
       }
-
       var retMsg = {
         msg : "Success"
-      }
-    
+      } 
       return new Response(JSON.stringify(retMsg), { status: 201, headers: headers });
     } else {
       // respond with method not allowed
@@ -43,50 +34,3 @@ export async function onRequest(context) {
     throw new Error(`temp is ` + body);
   }
 }
-
-
-
-
-
-
-// export async function onRequest(context) {
-//   let kList = (await context.env.dataPoints.list()).keys;
-//   let numberInList = kList.length;
-
-//   console.log(await context);
-//   let res = await context.env.dataPoints.put(numberInList, body);
-//   console.log(res);
-//   const headers = {
-//     Allow: 'OPTIONS, GET, HEAD, POST',
-//     'Access-Control-Allow-Origin': '*',
-//     'Content-type': 'application/json',
-//   }
-//   await context.env.dataPoints.put(numberInList, body);
-//   return new  Response(res, { status: 200, headers: headers });
-// }
-
-
-// export default async function gatherResponse(response) {
-//   const { headers } = response;
-//   const contentType = headers.get("content-type") || "";
-//   if (contentType.includes("application/json")) {
-//     return JSON.stringify(await response.json());
-//   } else if (contentType.includes("application/text")) {
-//     return response.text();
-//   } else if (contentType.includes("text/html")) {
-//     return response.text();
-//   } else {
-//     return response.text();
-//   }
-// }
-
-// const init = {
-//   body: JSON.stringify(body),
-//   method: "PUT",
-//   headers: {
-//     "content-type": "application/json;charset=UTF-8",
-//   },
-// };
-// const response = await fetch(url, init);
-// const results = await gatherResponse(response);
-// return new Response(results, init);
