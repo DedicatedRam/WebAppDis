a56c084a6ef1bdb9b929eb25ec3c28e30748a05e
DedicatedRam <65246530+DedicatedRam@users.noreply.github.com>
2023-08-19 10:42:45 +0100

    debug data returned from add new user

--- scriptOther.js
+++ scriptOther.js
@@ -62,14 +62,25 @@ function register() {
   };
   console.log(JSON.stringify(jsonObject));
 
-  fetch("https://cas-4d0.pages.dev/putUser",{
-    method: "POST",
-    body: JSON.stringify(jsonObject),
+  fetch("https://cas-4d0.pages.dev/putUser", {
+  method: "POST",
+  body: JSON.stringify(jsonObject),
+})
+  .then((response) => {
+    // Log the response content
+    return response.text(); // or response.blob() if it's not text
   })
-  .then((response) => response.json())
-    .then((data) =>{
+  .then((content) => {
+    console.log("Response Content:", content);
+    // Now, try to parse the content as JSON
+    try {
+      const data = JSON.parse(content);
       console.log(data);
-    }).catch((error) => console.error(error));
+    } catch (error) {
+      console.error("Error parsing JSON:", error);
+    }
+  })
+  .catch((error) => console.error(error));
 
   form.reset();
 }
